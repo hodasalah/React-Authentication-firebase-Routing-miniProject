7fcd37053aba6e8016b6e2b27b47d3bd8afb9c53
hodasalah <hodasalah35@gmail.com>
2022-01-16 15:43:59 +0300

    fetch data

---
+++ utils/fetchApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+//baseUrl
+export const baseURL = "https://bayut.p.rapidapi.com";
+// fetching data func
+export const fetchApi = async (url) => {
+	const { data } = await axios.get(url, {
+		headers: {
+			"x-rapidapi-host": "bayut.p.rapidapi.com",
+			"x-rapidapi-key":
+				"d5c75b0f1dmsh0a443d631462b9ap1031bdjsn8f9a2123284b",
+		},
+	});
+
+	return data;
+};
