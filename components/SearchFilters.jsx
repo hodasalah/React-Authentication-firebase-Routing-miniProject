86379ce65aa2f80712aad161b1de4c0cbb2f9416
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2022-03-18 01:04:11 +0300

    add filterData and start coding searchFilters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
 
 import {
 	flex,
